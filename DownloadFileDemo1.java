c894a2caa7f8f324b7132ac74980eb0c0bb31b4d
enohsaleonardoperez <enohsa.leonardo.perez@gmail.com.ar>
2015-08-24 17:04:50 -0300

    ..

--- DownloadFileDemo1.java
+++ DownloadFileDemo1.java
@@ -125,7 +125,7 @@ public class DownloadFileDemo1 extends Activity {
         if ( pb.getProgress()==100) {
             runOnUiThread(new Runnable() {
                 public void run() {
-
+                    123456
                     File pdfFile = new File(Environment.getExternalStorageDirectory() + "maven.pdf");  // -> filename = maven.pdf
                     Uri path = Uri.fromFile(pdfFile);
                     Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
